74a2bb9cfec92c4ed061aa5e5bd73420467bea4a
Bsgitt <boonnhon123@gmail.com>
2020-07-15 21:07:38 +0700

    initial-backend-side

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const colers = require("colors");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+
+dotenv.config({ path: "./config/config.env" });
+
+connectDB();
+const trasactions = require("./routes/transactions");
+
+const app = express();
+
+app.use(express.json());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use("/api/v1/transactions", trasactions);
+
+//set port
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
